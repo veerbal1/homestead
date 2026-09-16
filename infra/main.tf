8a345472c48bbeb484885dbafb52f24329bb48f8
veerbal1 <veerbalsingh1@gmail.com>
2026-09-16 22:29:09 +0530

    Managing AWS instances using teraform

---
+++ infra/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "homestead"
+}
+
+resource "aws_security_group" "homestead" {
+  name        = "homestead-tf"
+  description = "homestead app box"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 = all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ssm_parameter" "al2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_instance" "homestead" {
+  ami                    = data.aws_ssm_parameter.al2023.value
+  instance_type          = "t3.small"
+  key_name               = "homestead"
+  vpc_security_group_ids = [aws_security_group.homestead.id]
+
+  tags = {
+    Name = "homestead"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.homestead.public_ip
+}
